cac68da6e7b9c01c7e370f5f00df80de8ab8b684
razvan <razvandananau@gmail.com>
2022-09-14 15:33:45 +0300

    Added register + swagger

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -15,8 +15,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        #  add your swagger doc title
+        title="Showroom API",
+        #  version of the swagger doc
+        default_version='v1',
+        # first line that appears on the top of the doc
+        description="Test description",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('profiles_api.urls'))
+    path('api/', include('profiles_api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
